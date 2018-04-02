d554d82b75f9fe9921ed425cd68c58a25ba41e39
ArtiomAJ <35456697+ArtiomAJ@users.noreply.github.com>
2018-04-02 22:08:54 +0300

    Update ruby.rb

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,10 +1,8 @@
-yum_package 'epel-release' do
-  action :install
+package 'epel-release' do
 end
 
 execute 'install-ruby-version-manager' do
  command "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
- ignore_failure true
  action :run
 end
 
